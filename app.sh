da3dc656144f38b919a273931c5939639aa038a6
Johanna Herrmann <johanna.herrmann@mailbox.org>
2024-04-07 23:01:46 +0200

    feat: generation (draft)

--- app.sh
+++ app.sh
@@ -1,1 +1,41 @@
 #!/bin/sh
+
+spacer="\n\n\n"
+
+# alwys use /opt/work as workdirectory inside
+if [ -d "/github/workspace" ]
+then
+    ln -nsf /github/workspace /opt/work
+fi
+workdir=/opt/work
+
+# extract base iso
+mkdir iso
+mkdir squash
+mkdir -p extracted/run/live/medium/
+mount -o loop $workdir/clonezilla.iso iso
+mount -t squashfs iso/live/filesystem.squashfs squash
+rsync -auvx --progress squash/ extracted/
+rsync -auvx --progress iso/ extracted/run/live/medium/
+
+# add extra files if given
+if [ -d "$workdir/extra" ]
+then
+    mkdir -p extracted/run/live/medium/live/extra
+    rsync -auvx --progress "$workdir/extra" extracted/run/live/medium/live/extra/
+fi
+
+# copy generator
+cp generate.sh extracted/root/
+chmod a+x extracted/root/generate.sh
+
+# mount workdir
+mkdir -p extracted/root/work
+mount --bind "$workdir" extracted/root/work
+
+# dev and proc binding
+mount --bind /dev extracted/dev
+mount --bind /proc extracted/proc
+
+# generate
+chroot extracted /root/generate.sh
--- generate.sh
+++ generate.sh
@@ -1,1 +1,7 @@
-#!/bin/sh
+#!/bin/bash
+
+cd /root/work
+
+export PATH="$PATH:/usr/sbin"
+
+ocs-iso -g en_US.UTF-8 -k NONE -a clonezilla_customized -s -m custom-ocs
