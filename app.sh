9c011771d50a21aae607c744c6b538f40daed4d2
Johanna Herrmann <johanna.herrmann@mailbox.org>
2024-04-14 19:58:31 +0200

    feat: improves output

--- app.sh
+++ app.sh
@@ -1,40 +1,47 @@
 #!/bin/sh
 
+# preparations
 spacer="\n\n\n"
-
-# always use /opt/work as workdirectory inside
 if [ -d "/github/workspace" ]
 then
-    ln -nsf /github/workspace /opt/work
+    echo "preparing workdir"
+    ln -nsf /github/workspace/$1 /opt/work 2>/dev/null
 fi
 workdir=/opt/work
 
-# extract base iso
+# extracting iso file
+echo "extracting iso file"
 mkdir iso
 mkdir extracted
-xorriso -osirrox on -indev $workdir/clonezilla.iso -extract / iso
-unsquashfs -d extracted iso/live/filesystem.squashfs
+xorriso -osirrox on -indev $workdir/clonezilla.iso -extract / iso >/dev/null
+
+# extracting file system
+echo "extracting file system"
+unsquashfs -d extracted iso/live/filesystem.squashfs >/dev/null
+
+# bind-mounting iso, extra files and workdir
+echo "bind-mounting iso, extra files and workdir"
 mkdir -p extracted/run/live/medium
 mount --bind iso extracted/run/live/medium
-
-# add extra files if given
 if [ -d "$workdir/extra" ]
 then
     mkdir -p extracted/run/live/medium/live/extra
     mount --bind "$workdir/extra" extracted/run/live/medium/live/extra
 fi
-
-# copy generator
-cp generate.sh extracted/root/
-chmod a+x extracted/root/generate.sh
-
-# mount workdir
 mkdir -p extracted/root/work
 mount --bind "$workdir" extracted/root/work
 
-# dev and proc binding
+# adding generator script
+echo "adding generator script"
+cp generate.sh extracted/root/
+chmod a+x extracted/root/generate.sh
+
+# bind-mounting dev and proc
+echo "bind-mounting dev and proc"
 mount --bind /dev extracted/dev
 mount --bind /proc extracted/proc
 
 # generate
+echo "running generator script"
 chroot extracted /root/generate.sh
+echo "\n\nCreated custom image successfully"
--- generate.sh
+++ generate.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 cd /root/work
-
 export PATH="$PATH:/usr/sbin"
-
-ocs-iso -g en_US.UTF-8 -k NONE -a clonezilla_customized -s -m custom-ocs
+ocs-iso -g en_US.UTF-8 -k NONE -a clonezilla_customized -s -m custom-ocs >/dev/null
