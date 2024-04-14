faa22729e540413c520d9d70bdb7f24e88a502d2
Johanna Herrmann <johanna.herrmann@mailbox.org>
2024-04-14 20:56:34 +0200

    fix: improves error handling

--- app.sh
+++ app.sh
@@ -1,23 +1,36 @@
 #!/bin/sh
 
 # preparations
-spacer="\n\n\n"
+RED='\033[0;31m'
+NC='\033[0m'
 if [ -d "/github/workspace" ]
 then
     echo "preparing workdir"
     ln -nsf /github/workspace/$1 /opt/work 2>/dev/null
+    if [ -z "$1" ] || [ ! -d "/github/workspace/$1" ]
+    then
+        echo "${RED}ERROR: Missing or invalid work director provided${NC}"
+        exit 1
+    fi
 fi
 workdir=/opt/work
+
+# check workdir
+if [ ! -f "$workdir/clonezilla.iso" ] || [ ! -f "$workdir/custom-ocs" ]
+then
+    echo "${RED}ERROR: Missing base image or custom-ocs. Correct work director provided?"
+    exit 1
+fi
 
 # extracting iso file
 echo "extracting iso file"
 mkdir iso
 mkdir extracted
-xorriso -osirrox on -indev $workdir/clonezilla.iso -extract / iso >/dev/null
+xorriso -osirrox on -indev $workdir/clonezilla.iso -extract / iso >/dev/null || exit
 
 # extracting file system
 echo "extracting file system"
-unsquashfs -d extracted iso/live/filesystem.squashfs >/dev/null
+unsquashfs -d extracted iso/live/filesystem.squashfs >/dev/null || exit
 
 # bind-mounting iso, extra files and workdir
 echo "bind-mounting iso, extra files and workdir"
@@ -38,10 +51,10 @@ chmod a+x extracted/root/generate.sh
 
 # bind-mounting dev and proc
 echo "bind-mounting dev and proc"
-mount --bind /dev extracted/dev
-mount --bind /proc extracted/proc
+mount --bind /dev extracted/dev || exit
+mount --bind /proc extracted/proc || exit
 
 # generate
 echo "running generator script"
-chroot extracted /root/generate.sh
+chroot extracted /root/generate.sh || exit
 echo "\n\nCreated custom image successfully"
